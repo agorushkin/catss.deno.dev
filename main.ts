043aea9592ee64e86db13bc218c4f412195717d6
Arsenii Gorushkin <arsenii.gorushkin@gmail.com>
2022-05-01 14:36:32 +1200

    feat: clicking on image copies it to clipboard

--- main.ts
+++ main.ts
@@ -4,11 +4,22 @@ const server = new Server();
 
 server.static('/public/', './');
 
-const main = server.on();
+const main = server.on('/{gif}?');
 main(({ respond }) => {
   fetch(`file://${ Deno.cwd( ) }/main.html`)
     .then(async (data) => respond({ body: await data.arrayBuffer(), headers: { 'content-type': 'text/html' } }))
     .catch(() => respond({ body: '<h1>Error</h1>', status: 404, headers: { 'content-type': 'text/html' } }));
+});
+
+const get = server.on('/get/:name');
+get(async ({ respond, params: { name } }) => {
+  const request = await fetch(`https://cdn2.thecatapi.com/images/${ name }`);
+  const blob    = await request.blob();
+  const uint    = new Uint8Array(await blob.arrayBuffer());
+  
+  const contentType = request.headers.get('content-type')!;
+
+  respond({ body: uint, headers: { 'content-type': contentType } });
 });
 
 server.listen();
